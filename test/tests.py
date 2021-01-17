b50bf736001385af6b8c4559ffbd3595bdf65c6f
LadyNick <nnikki1013@gmail.com>
2021-01-17 01:05:30 -0800

    Challenge

--- test/tests.py
+++ test/tests.py
@@ -18,7 +18,7 @@
 
 tests = [
     {'description': 'PINA: 0x01 => PORTC: 0x01',
-    'steps': [ {'inputs': [('PINA',0x01)], 'iterations': 5 } ],
+    'steps': [ {'inputs': [('PINA',0x01) ], 'iterations': 5 } ],
     'expected': [('PORTC',0x01)],
     },
     {'description': 'PINA: 0x03 => PORTC: 0x02',
