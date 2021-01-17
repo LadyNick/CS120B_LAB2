19a2730a0530e286c76d38b91a09135752375f15
LadyNick <72218995+LadyNick@users.noreply.github.com>
2021-01-17 13:46:27 -0800

    Update nnava026_lab2_part3.c

--- turnin/nnava026_lab2_part3.c
+++ turnin/nnava026_lab2_part3.c
@@ -30,7 +30,7 @@ while(1) {
 		// 2) Perform computation
 		//Where each pin is a parking space, PORTC should output binary number of available spots
 		
-		cntavail = tmpA0 + tmpA1 + tmpA2 + tmpA3;
+		cntavail = 0x04 - tmpA0 - tmpA1 - tmpA2 - tmpA3;
 		if(cntavail == 0x04){
 			cntavail = (cntavail | 0x01 << 7); 
 		}	
